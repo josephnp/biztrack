9c3a93ad2a85706f386f1ba8ab5ab898e9aeef41
KevinC76 <xckevinxc67@gmail.com>
2025-05-19 15:20:25 +0700

    add AccessMenu model

--- src/main/java/com/biztrack/business_expense_tracker/model/AccessMenu.java
+++ src/main/java/com/biztrack/business_expense_tracker/model/AccessMenu.java
@@ -1,4 +1,34 @@
 package com.biztrack.business_expense_tracker.model;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class AccessMenu{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "RoleID", nullable = false, foreignKey = @ForeignKey(name = "FK_Users_Role"))
+    private Long roleId;
+
+    @Column(name = "Menu")
+    private String menu;
+
+    @Column(name = "CreatedBy", nullable = false, updatable = false)
+    private UUID createdBy;
+
+    @Column(name = "ModifiedBy", insertable = false)
+    private LocalDateTime createdDate;
+
+    @Column(name = "ModifiedBy", insertable = false)
+    private UUID modifiedBy;
+
+    @UpdateTimestamp
+    @Column(name = "ModifiedDate", insertable = false)
+    private LocalDateTime modifiedDate;
 }
