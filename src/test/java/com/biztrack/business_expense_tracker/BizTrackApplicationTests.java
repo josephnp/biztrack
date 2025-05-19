4d9f7c0258bdbf671ed2ac06fc87e9022ddf5267
josephnp <josephnp18.jnp@gmail.com>
2025-05-19 10:23:49 +0700

    first commit

---
+++ src/test/java/com/biztrack/business_expense_tracker/BizTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biztrack.business_expense_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
