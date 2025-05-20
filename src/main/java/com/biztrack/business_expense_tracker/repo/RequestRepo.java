bd5df344fc7daded94627961ef3204c0a7535254
KevinC76 <xckevinxc67@gmail.com>
2025-05-20 12:31:49 +0700

    feat: create report and request repo and request

--- src/main/java/com/biztrack/business_expense_tracker/repo/RequestRepo.java
+++
@@ -1,7 +0,0 @@
-package com.biztrack.business_expense_tracker.repo;
-
-import com.biztrack.business_expense_tracker.model.Request;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequestRepo extends JpaRepository<Request, Long> {
-}
