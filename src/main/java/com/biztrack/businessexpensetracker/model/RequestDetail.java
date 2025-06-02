c0c523667b85e463d3163c3dd80610e850ad8325
Lyongky Suryono Taniaji <lyongkysuryonotaniaji@gmail.com>
2025-06-02 21:48:15 +0700

    add request detail and report detail model
    (cherry picked from commit b9ad6c36bcfee973cd44e1ceeaa3cb5f05863a95)

---
+++ src/main/java/com/biztrack/businessexpensetracker/model/RequestDetail.java
@@ -0,0 +1,104 @@
+package com.biztrack.businessexpensetracker.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "RequestDetail")
+public class RequestDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "RequestID", foreignKey = @ForeignKey(name = "fk-request-detail"))
+    private Request request;
+
+    @Column(name = "Amount")
+    private Double amount;
+
+    @Column(name = "Description")
+    private String description;
+
+    @Column(name = "CreatedBy", nullable = false, updatable = false)
+    private Long createdBy;
+
+    @CreationTimestamp
+    @Column(name = "CreatedDate", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @Column(name = "ModifiedBy", insertable = false)
+    private Long modifiedBy;
+
+    @UpdateTimestamp
+    @Column(name = "ModifiedDate", insertable = false)
+    private LocalDateTime modifiedDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Request getRequest() {
+        return request;
+    }
+
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Long getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(Long modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
